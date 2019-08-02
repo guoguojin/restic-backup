f762f925577fe52df7e4a1a1c0d968ef02c18648
Tan Quach <tan.quach@birchwoodlangham.com>
2019-08-02 10:02:20 +0100

    Fixed error in setup script

--- setup-restic-backup.sh
+++ setup-restic-backup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo cp ./restic-backup-remote.service ./restic-backuo-remote.timer ./restic-backup.service ./restic-backup.timer /usr/lib/systemd/user
+sudo cp ./restic-backup-remote.service ./restic-backup-remote.timer ./restic-backup.service ./restic-backup.timer /usr/lib/systemd/user
 systemctl --user enable restic-backup.service
 systemctl --user enable restic-backup.timer
 systemctl --user enable restic-backup-remote.service
