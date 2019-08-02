e96978f541b882a4279c57da1ab432a8e5bb891f
Tan Quach <t@nqua.ch>
2019-08-02 13:39:04 +0100

    Updated setup script to link restic-backup-files and restic-excludes to the appropriate places

--- setup-restic-backup.sh
+++ setup-restic-backup.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
 sudo cp ./restic-backup-remote.service ./restic-backup-remote.timer ./restic-backup.service ./restic-backup.timer /usr/lib/systemd/user
+
 systemctl --user enable restic-backup.service
 systemctl --user enable restic-backup.timer
 systemctl --user enable restic-backup-remote.service
 systemctl --user enable restic-backup-remote.timer
+
+if [[ ! -f $HOME/restic-backup-files ]]; then
+    ln -s $PWD/restic-backup-files $HOME/restic-backup-files
+fi
+
+if [[ ! -f $HOME/restic-excludes ]]; then
+    ln -s $PWD/restic-excludes $HOME/restic-excludes
+fi
+
